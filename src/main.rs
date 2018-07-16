99f08fb5c7e652533dad6b7ca6e053303ca7f36a
Matthew Schauer <matthew.schauer@e10x.net>
2018-07-16 17:45:33 -0400

    Allow changing templating characters and ignoring template failures

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-#[macro_use] extern crate error_chain; // https://stevedonovan.github.io/rust-gentle-intro/6-error-handling.html#error-chain-for-serious-errors
-#[macro_use] extern crate clap; // https://docs.rs/clap/2.32.0/clap/
-#[macro_use] extern crate nom; // https://stevedonovan.github.io/rust-gentle-intro/nom-intro.html
-extern crate yaml_rust; // http://chyh1990.github.io/yaml-rust/
+#[macro_use] extern crate error_chain;
+#[macro_use] extern crate clap;
+#[macro_use] extern crate nom;
+extern crate yaml_rust;
 
 use std::fs::File;
 use std::io::Read;
@@ -11,16 +11,14 @@ use yaml_rust::Yaml;
 mod errors { error_chain!{} }
 use errors::*;
 
-// https://mustache.github.io/mustache.5.html
-
 // TODO
+// Split into multiple files; add TESTS and DOCS
 // https://stackoverflow.com/questions/46876879/how-do-i-create-a-streaming-parser-in-nom
-// Allow comments to contain {{ and }} -- that is, do the nesting calculation for them as well
-// Error if there are unmatched conds (missing {{/}}s) rather than implicitly closing them at the end
-// User-friendly errors on template parsing failures
+// Error if there are unmatched conds (missing {{/}}s) rather than implicitly closing them at the end (related to next line)
 // Allow text to be included after the / in EndSub.  Either ignore it or require it to match the start text
 // Options to collapse whitespace?
 // All the error messages need to be a lot nicer
+// Allow escaping so that {{ can appear in the document if someone really needs it to
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum YamlPathElem {
@@ -29,6 +27,8 @@ pub enum YamlPathElem {
 	Up,
 	Root
 }
+
+type YamlPath = Vec<YamlPathElem>;
 
 named!(yaml_path<&str, YamlPath>,
 	do_parse!(
@@ -40,10 +40,10 @@ named!(yaml_path<&str, YamlPath>,
 					ws!(
 						alt!(
 							do_parse!(n: recognize!(nom::digit) >> (YamlPathElem::DownArray(n.parse::<i64>().expect("Failed to parse digits as number")))) | // TODO Don't use expect, here or anywhere else
-							do_parse!(name: recognize!(nom::alphanumeric) >> (YamlPathElem::DownObject(name.to_string()))) |
+							do_parse!(name: recognize!(nom::alphanumeric) >> (YamlPathElem::DownObject(name.to_string()))) | // FIXME YAML keys can consist of any character, properly escaped.  So we'll have to be more robust about this.
 							do_parse!(tag_s!("&") >> (YamlPathElem::Up))
 						)
-					) // FIXME YAML keys can consist of any character, properly escaped.  So we'll have to be more robust about this.
+					)
 				)
 			)
 		) >>
@@ -56,8 +56,6 @@ named!(yaml_path<&str, YamlPath>,
 	)
 );
 
-type YamlPath = Vec<YamlPathElem>;
-
 #[derive(Debug, PartialEq)]
 pub enum Token {
 	Literal(String),
@@ -69,24 +67,27 @@ pub enum Token {
 	Comment(String),
 }
 
-named!(template_sub<&str, Token>,
-	delimited!(
-		tag_s!("{{"),
-		switch!(opt!(one_of!("#/^!?")),
-			None => do_parse!(path: yaml_path >> (Token::DirectSub(path))) |
-			Some('#') => do_parse!(path: yaml_path >> (Token::CondSub(path))) |
-			Some('^') => do_parse!(path: yaml_path >> (Token::InvSub(path))) |
-			Some('/') => do_parse!((Token::EndSub)) | // FIXME How do I return Token::EndSub without this pointless do_parse?
-			Some('?') => do_parse!(n: opt!(nom::digit) >> (Token::KeySub(n.map(|x| x.parse::<i64>().expect("Failed to parse digits as number")).unwrap_or(0)))) |
-			Some('!') => do_parse!(text: take_until!("}}") >> (Token::Comment(text.to_string())))
-		),
-		tag_s!("}}")
+named_args!(template_sub<'a>(open: &str, close: &str) <&'a str, Token>,
+	alt!(
+		do_parse!(tag_s!(open) >> tag_s!("!--") >> text: take_until!(&format!("--{}", close)[..]) >> tag_s!("--") >> tag_s!(close) >> (Token::Comment(text.to_string()))) |
+		delimited!(
+			tag_s!(open),
+			switch!(opt!(one_of!("#/^!?")),
+				None => do_parse!(path: yaml_path >> (Token::DirectSub(path))) |
+				Some('#') => do_parse!(path: yaml_path >> (Token::CondSub(path))) |
+				Some('^') => do_parse!(path: yaml_path >> (Token::InvSub(path))) |
+				Some('/') => do_parse!((Token::EndSub)) | // FIXME How do I return Token::EndSub without this pointless do_parse?
+				Some('?') => do_parse!(n: opt!(nom::digit) >> (Token::KeySub(n.map(|x| x.parse::<i64>().expect("Failed to parse digits as number")).unwrap_or(0)))) |
+				Some('!') => do_parse!(text: take_until!(close) >> (Token::Comment(text.to_string())))
+			),
+			tag_s!(close)
+		)
 	)
 );
 
-named!(template_literal<&str, Token>,
+named_args!(template_literal<'a>(open: &str) <&'a str, Token>,
 	do_parse!(
-		content: alt!(take_until!("{{") | nom::rest_s) >> // FIXME How do I ensure here that ALL input is processed?
+		content: alt!(take_until!(open) | nom::rest_s) >> // FIXME How do I ensure here that ALL input is processed?
 		(Token::Literal(content.to_string()))
 	)
 );
@@ -99,11 +100,11 @@ named!(yaml_block<&str, Vec<Yaml>>,
 	)
 );
 
-named!(document<&str, (Option<Vec<Yaml>>, Vec<Token>)>,
+named_args!(document<'a>(open: &str, close: &str) <&'a str, (Option<Vec<Yaml>>, Vec<Token>)>,
 	tuple!(
 		opt!(yaml_block),
 		many0!(
-			alt!(complete!(template_sub) | complete!(template_literal))
+			alt!(complete!(call!(template_sub, open, close)) | complete!(call!(template_literal, open)))
 		)
 	)
 );
@@ -150,15 +151,14 @@ fn yaml_bool(yaml: &Yaml) -> bool {
 	}
 }
 
-fn yaml_string(yaml: &Yaml) -> Result<String> {
+fn yaml_string(yaml: &Yaml, ignore: bool) -> Result<String> {
 	match yaml {
 		Yaml::Real(x) => Ok(x.to_string()),
 		Yaml::Integer(x) => Ok(x.to_string()),
 		Yaml::String(x) => Ok(x.to_string()),
 		Yaml::Boolean(x) => Ok(x.to_string()),
 		Yaml::Null => Ok("".to_string()),
-		//_ => Ok("".to_string()), // TODO Make this behavior configurable
-		_ => Err(Error::from("Can't stringify type")), // TODO This error message (and a lot of others) needs to be better
+		_ => if ignore { Ok("".to_string()) } else { Err(Error::from("Can't stringify type")) }, // TODO This error message (and a lot of others) needs to be better
 	}
 }
 
@@ -196,25 +196,25 @@ fn build_tree(tokens: &[Token]) -> (usize, Vec<Node>) {
 	(i, ret)
 }
 
-fn render(values: &Yaml, tree: &[Node], context: &YamlPath) -> Result<String> {
+fn render(values: &Yaml, tree: &[Node], context: &YamlPath, ignore: bool) -> Result<String> {
 	let mut ret = "".to_string();
 	for node in tree {
 		let cur = match node {
 			Node::Literal(ref s) => s.to_string(),
-			Node::DirectSub(ref path) => yaml_string(yaml_get(values, &yaml_pathjoin(&vec![context, path][..]))).chain_err(|| "Couldn't stringify value")?,
+			Node::DirectSub(ref path) => yaml_string(yaml_get(values, &yaml_pathjoin(&vec![context, path][..])), ignore).chain_err(|| "Couldn't stringify value")?,
 			Node::CondSub(ref path, direct, ref children) => {
 				let abspath = &yaml_pathjoin(&vec![context, path][..]);
 				let target = yaml_get(values, abspath);
 				if yaml_bool(target) && *direct {
 					match target {
 						// TODO Don't unwrap; don't use as_str() (this will require doing something about the non-string key case); try to pull out common parts of these lines
-						Yaml::Hash(ref contents) => contents.keys().map(|k| render(values, children, &yaml_pathjoin(&vec![abspath, &vec![YamlPathElem::DownObject(k.as_str().unwrap().to_string())]])).unwrap()).fold("".to_string(), |mut ret, cur| { ret.push_str(&cur); ret }),
-						Yaml::Array(ref contents) => (0..contents.len() as i64).into_iter().map(|i| render(values, children, &yaml_pathjoin(&vec![abspath, &vec![YamlPathElem::DownArray(i)]])).unwrap()).fold("".to_string(), |mut ret, cur| { ret.push_str(&cur); ret }),
-						_ => render(values, children, abspath)?,
+						Yaml::Hash(ref contents) => contents.keys().map(|k| render(values, children, &yaml_pathjoin(&vec![abspath, &vec![YamlPathElem::DownObject(k.as_str().unwrap().to_string())]]), ignore).unwrap()).collect(),
+						Yaml::Array(ref contents) => (0..contents.len() as i64).into_iter().map(|i| render(values, children, &yaml_pathjoin(&vec![abspath, &vec![YamlPathElem::DownArray(i)]]), ignore).unwrap()).collect(),
+						_ => render(values, children, abspath, ignore)?,
 					}
 				}
 				else if ! yaml_bool(target) && ! *direct {
-					render(values, children, abspath)?
+					render(values, children, abspath, ignore)?
 				}
 				else { "".to_string() }
 			},
@@ -229,15 +229,31 @@ fn render(values: &Yaml, tree: &[Node], context: &YamlPath) -> Result<String> {
 	Ok(ret)
 }
 
+fn matching_delim(open: &str) -> String { // TODO Not Unicode-aware.  Is it practical?
+	fn flip(c: char) -> char {
+		match c {
+			'(' => ')', '[' => ']', '{' => '}', '<' => '>',
+			')' => '(', ']' => '[', '}' => '{', '>' => '<',
+			x => x,
+		}
+	}
+	open.chars().map(flip).collect()
+}
+
 fn run() -> Result<()> {
 	let args = clap_app!(tpl =>
 		(about: "Simple multi-purpose template engine")
 		(@arg input: * index(1) "File to be templated")
 		(@arg values: -f [file] "YAML file of template values")
+		(@arg delim_a: -A [delim] "Opening delimiter for tags (default \"{{\")")
+		(@arg delim_b: -B [delim] "Closing delimiter for tags (default \"}}\")")
+		(@arg ignore: -i "Ignore rather than erroring on invalid subtitutions")
 	).get_matches();
 
 	let input = read_file(args.value_of("input").unwrap()).chain_err(|| "Failed to get input")?;
-	let template = document(&input).expect("Failed to parse template"); // FIXME Why doesn't the borrow checker like this? .chain_err(|| "Failed to parse template")?;
+	let delim_open = args.value_of("delim_a").unwrap_or("{{").to_string();
+	let delim_close = args.value_of("delim_b").map(|x| x.to_string()).unwrap_or(matching_delim(&delim_open));
+	let template = document(&input, &delim_open, &delim_close).expect("Failed to parse template"); // FIXME Why doesn't the borrow checker like this? .chain_err(|| "Failed to parse template")?;
 	let mut tokens = (template.1).1;
 	tokens.push(Token::Literal(template.0.to_string())); // FIXME This hack will go away when I figure out how to make Nom parse all input
 	let tree = build_tree(&tokens).1;
@@ -247,7 +263,7 @@ fn run() -> Result<()> {
 		.or_else(|| yaml)
 		.ok_or(Error::from("Values are required either inline in the input or using the values flag"))?;
 	let values = &values[0]; // TODO What should we do if there are multiple streams in the file?  Ignore them?
-	print!("{}", render(values, &tree, &vec![])?);
+	print!("{}", render(values, &tree, &vec![], args.is_present("ignore"))?);
 	Ok(())
 }
 
